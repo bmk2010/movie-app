b3bfa61a3db47a18845428634c93cd39d7ffa64a
bmk2010 <profiy70@gmail.com>
2024-10-01 17:19:57 +0500

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import LoginPage from "./pages/Auth";
 import Home from "./pages/Home";
-import SmsPage from "./pages/Sms";
+import SmsPage from "./pages/Sms"
 import ProfilePage from "./pages/Profile";
-import Seans from './pages/Seans';
+import SeansPage from "./pages/seans";
 import DetailPage from "./pages/Detail";
-
-const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}> 
-      <div className="max-w-[1360px] w-full mx-auto">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/login/sms" element={<SmsPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/seans" element={<Seans />} />
-            <Route path="/:id" element={<DetailPage />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </QueryClientProvider>
+    <div className="max-w-[1360px] w-full mx-auto">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/login/sms" element={<SmsPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/seans" element={<SeansPage />} />
+          <Route path="/:id" element={<DetailPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
